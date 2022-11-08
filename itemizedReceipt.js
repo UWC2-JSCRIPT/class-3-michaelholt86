bede2956940b39bef5675276bcc88d154ed19782
Michael Holt <Michaelh@andreacola.com>
2022-11-08 16:48:27 -0700

    Added Tax

--- itemizedReceipt.js
+++ itemizedReceipt.js
@@ -5,11 +5,17 @@
 
 const logReceipt = function(...menuItems){
     let totalPrice = 0
+    let tax = .086
     menuItems.forEach((menuItem)=>{
       console.log(`${menuItem.descr} : ${menuItem.price}`)
       totalPrice += menuItem.price
     })
-    console.log(totalPrice)
+    console.log("Sub-Total: $" + totalPrice.toFixed(2))
+    console.log("Tax: $"+ (totalPrice * tax).toFixed(2))
+    console.log("Total: $" + ((totalPrice * tax) + totalPrice).toFixed(2))
+
+ 
+   
 } 
 
 // Check
@@ -17,9 +23,9 @@ logReceipt(
   { descr: 'Burrito', price: 5.99 },
   { descr: 'Chips & Salsa', price: 2.99 },
   { descr: 'Sprite', price: 1.99 },
-  { descr: 'Burrito', price: 5.99 },
-  { descr: 'Chips & Salsa', price: 2.99 },
-  { descr: 'Sprite', price: 1.99 }
+  { descr: 'Nachos', price: 4.99 },
+  { descr: 'Agua Fresca', price: 3.99 },
+  { descr: 'Carne Asada Fries', price: 11.99 }
 );
 // should log something like:
 // Burrito - $5.99
